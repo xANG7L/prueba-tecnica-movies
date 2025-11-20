f2df180b8688c04e1ea41120b176424d8c132ac8
ANGEL <ang7l.manciaa@gmail.com>
2025-11-20 14:26:17 -0600

    Algunos ajustes en la personalizacion!

--- src/app/components/movies/movie-detail/movie-detail.component.ts
+++ src/app/components/movies/movie-detail/movie-detail.component.ts
@@ -2,11 +2,13 @@ import { Component, inject, input, signal } from '@angular/core';
 import { MovieDetail } from '../../../models/movie-details';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { MovieService } from '../../../services/movie.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-movie-detail.component',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './movie-detail.component.html',
   styleUrl: './movie-detail.component.css',
 })
@@ -21,9 +23,4 @@ export class MovieDetailComponent {
     stream: ({ params }) => this.movieService.getMovieById(params.id)
   });
 
-  movie = this.movieResource.value;
-
-  loading = this.movieResource.isLoading;
-
-  error = this.movieResource.error;
 }
